996ff3a707859f6c0431292545383fda56f90bcb
mohammadmahdi.roozitalab <mohammadmahdi.roozitalab@snapp.cab>
2020-11-19 06:09:50 +0330

    Added some useful error to the library

---
+++ errors.go
@@ -0,0 +1,15 @@
+package helpers
+
+// This type represent invalid argument error
+type InvalidArgs struct{}
+
+func (this InvalidArgs) Error() string { return "Invalid Arguments" }
+
+// This type represent out of range error
+type OutOfRange struct{}
+
+func (this OutOfRange) Error() string { return "Value is out of range" }
+
+type OperationTimedOut struct{}
+
+func (this OperationTimedOut) Error() string { return "operation timed out" }
