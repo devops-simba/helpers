bb3a4a8b111e7a9fab42b8bd2e7346893242c710
mohammadmahdi.roozitalab <mohammadmahdi.roozitalab@snapp.cab>
2020-11-21 13:26:59 +0330

    Added ability to load CertAndKey from PEM files

--- tls.go
+++ tls.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"crypto"
 	"crypto/ecdsa"
+	"crypto/ed25519"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
@@ -10,6 +11,7 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"errors"
+	"io/ioutil"
 	"math/big"
 	"time"
 )
@@ -17,6 +19,13 @@ import (
 var (
 	UnsupportedEncryptionType = errors.New("Unsupported encryption type")
 	NoIssuerCertMustBeCA      = errors.New("When there is no issuer for the certificate it must be a CA")
+
+	ErrInvalidPEMFile      = errors.New("Invalid PEM file")
+	ErrNoCertificate       = errors.New("PEM file does not contains any certificate")
+	ErrNoKey               = errors.New("PEM file does not contains any private key")
+	ErrMultipleCertificate = errors.New("Found multiple certificates in PEM file")
+	ErrMultipleKey         = errors.New("Found multiple private key in PEM file")
+	ErrUnsupportedKeyType  = errors.New("Private key type is not supported")
 )
 
 type CryptoAlgorithm string
@@ -29,6 +38,7 @@ const (
 	ECDSA256 CryptoAlgorithm = "ECDSA256"
 	ECDSA384 CryptoAlgorithm = "ECDSA384"
 	ECDSA521 CryptoAlgorithm = "ECDSAP521"
+	ED25519  CryptoAlgorithm = "ED25519"
 )
 
 func CreatePrivateKey(algo CryptoAlgorithm) (crypto.PrivateKey, error) {
@@ -47,18 +57,23 @@ func CreatePrivateKey(algo CryptoAlgorithm) (crypto.PrivateKey, error) {
 		return ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
 	case ECDSA521:
 		return ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	case ED25519:
+		_, priv, err := ed25519.GenerateKey(rand.Reader)
+		return priv, err
 	default:
 		return nil, UnsupportedEncryptionType
 	}
 }
-func GetPublicKey(privKey crypto.PrivateKey) (crypto.PublicKey, error) {
-	switch k := privKey.(type) {
+func GetPublicKey(priv crypto.PrivateKey) (crypto.PublicKey, error) {
+	switch k := priv.(type) {
 	case *rsa.PrivateKey:
 		return k.Public(), nil
 
 	case *ecdsa.PrivateKey:
 		return k.Public(), nil
 
+	case *ed25519.PrivateKey:
+		return k.Public(), nil
 	default:
 		return nil, UnsupportedEncryptionType
 	}
@@ -92,9 +107,98 @@ func CreateX509Certificate(commonName string, isCA bool, expiryTime time.Time) (
 type CertAndKey struct {
 	Certificate *x509.Certificate
 	PrivateKey  crypto.PrivateKey
-	DER         []byte
 }
 
+func loadPEMBuffer(buffer []byte) (*x509.Certificate, crypto.PrivateKey, error) {
+	var cert *x509.Certificate
+	var key crypto.PrivateKey
+	var err error
+	var block *pem.Block
+
+	block, buffer = pem.Decode(buffer)
+	for block != nil && err == nil {
+		switch block.Type {
+		case "CERTIFICATE":
+			if cert != nil {
+				err = ErrMultipleCertificate
+			} else {
+				cert, err = x509.ParseCertificate(block.Bytes)
+			}
+
+		case "PRIVATE KEY":
+			if key != nil {
+				err = ErrMultipleKey
+			} else {
+				key, err = x509.ParsePKCS8PrivateKey(block.Bytes)
+			}
+
+		case "EC PRIVATE KEY":
+			if key != nil {
+				err = ErrMultipleKey
+			} else {
+				key, err = x509.ParseECPrivateKey(block.Bytes)
+			}
+
+		case "RSA PRIVATE KEY":
+			if key != nil {
+				err = ErrMultipleKey
+			} else {
+				key, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+			}
+
+		default:
+			// ignore other kind of blocks
+		}
+
+		block, buffer = pem.Decode(buffer)
+	}
+	if cert == nil && key == nil {
+		return nil, nil, ErrInvalidPEMFile
+	}
+	return cert, key, err
+}
+func loadPEM(file string) (*x509.Certificate, crypto.PrivateKey, error) {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return loadPEMBuffer(buffer)
+}
+func LoadCertAndKeyFromFile(file string) (*CertAndKey, error) {
+	cert, key, err := loadPEM(file)
+	if err != nil {
+		return nil, err
+	}
+
+	if cert == nil {
+		return nil, ErrNoCertificate
+	}
+	if key == nil {
+		return nil, ErrNoKey
+	}
+
+	return &CertAndKey{Certificate: cert, PrivateKey: key}, nil
+}
+func LoadCertAndKeyFromCertAndKey(certFile, keyFile string) (*CertAndKey, error) {
+	cert, _, err := loadPEM(certFile)
+	if err != nil {
+		return nil, err
+	}
+	if cert == nil {
+		return nil, ErrNoCertificate
+	}
+
+	_, key, err := loadPEM(keyFile)
+	if err != nil {
+		return nil, err
+	}
+	if key == nil {
+		return nil, ErrNoKey
+	}
+
+	return &CertAndKey{Certificate: cert, PrivateKey: key}, nil
+}
 func CreateCertificate(cert *x509.Certificate, privateKey crypto.PrivateKey, issuer *CertAndKey) (*CertAndKey, error) {
 	var err error
 	if issuer == nil && !cert.IsCA {
@@ -124,14 +228,21 @@ func CreateCertificate(cert *x509.Certificate, privateKey crypto.PrivateKey, iss
 		return nil, err
 	}
 
+	cert, err = x509.ParseCertificate(der)
+	if err != nil {
+		return nil, err
+	}
+
 	return &CertAndKey{
 		Certificate: cert,
 		PrivateKey:  privateKey,
-		DER:         der,
 	}, nil
 }
-func (this *CertAndKey) CertificatePEMBlock() *pem.Block {
-	return &pem.Block{Type: "CERTIFICATE", Bytes: this.DER}
+func (this *CertAndKey) CertificatePEMBlock() (*pem.Block, error) {
+	if this.Certificate.Raw == nil {
+		return nil, errors.New("Certificate missing DER information")
+	}
+	return &pem.Block{Type: "CERTIFICATE", Bytes: this.Certificate.Raw}, nil
 }
 func (this *CertAndKey) PrivateKeyPEMBlock() (*pem.Block, error) {
 	switch k := this.PrivateKey.(type) {
