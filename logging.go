50fefe1c609f8744905dd88fabe7709e4949dfe8
mohammadmahdi.roozitalab <mohammadmahdi.roozitalab@snapp.cab>
2020-11-29 08:55:59 +0330

    +Verbose logging

--- logging.go
+++ logging.go
@@ -87,11 +87,14 @@ func (this LogLevel) Format(format string) string {
 func (this LogLevel) String() string { return this.Format("n") }
 
 type LogFactory interface {
-	CreateLogger(name string) Logger
+	CreateLogger(name string, level *LogLevel, verbosityLevel *int) Logger
 }
 
 type Logger interface {
 	LogFactory
+
+	V(verbosityLevel int) bool
+	IsEnabled(level LogLevel) bool
 
 	Debug(message interface{})
 	Debugf(format string, args ...interface{})
@@ -103,6 +106,9 @@ type Logger interface {
 	Errorf(format string, args ...interface{})
 	Fatal(message interface{})
 	Fatalf(format string, args ...interface{})
+
+	Verbose(verbosityLevel int, message interface{})
+	Verbosef(verbosityLevel int, format string, args ...interface{})
 }
 
 type LogRecord struct {
@@ -113,18 +119,22 @@ type LogRecord struct {
 }
 
 type FileLogFactory struct {
-	dispatcher chan *LogRecord
-	format     *template.Template
-	output     *os.File
-	stopped    chan struct{}
+	dispatcher     chan *LogRecord
+	format         *template.Template
+	output         *os.File
+	stopped        chan struct{}
+	minimumLevel   LogLevel
+	verbosityLevel int
 }
 
-func NewFileLogFactory(format *template.Template, output *os.File) *FileLogFactory {
+func NewFileLogFactory(format *template.Template, output *os.File, minimumLogLevel LogLevel, verbosityLevel int) *FileLogFactory {
 	result := &FileLogFactory{
-		dispatcher: make(chan *LogRecord),
-		format:     format,
-		output:     output,
-		stopped:    make(chan struct{}),
+		dispatcher:     make(chan *LogRecord),
+		format:         format,
+		output:         output,
+		stopped:        make(chan struct{}),
+		minimumLevel:   minimumLogLevel,
+		verbosityLevel: verbosityLevel,
 	}
 
 	context := GetDefaultContext(output)
@@ -149,10 +159,18 @@ func NewFileLogFactory(format *template.Template, output *os.File) *FileLogFacto
 	return result
 }
 
-func (this *FileLogFactory) CreateLogger(name string) Logger {
+func (this *FileLogFactory) CreateLogger(name string, minimumLogLevel *LogLevel, verbosityLevel *int) Logger {
+	if minimumLogLevel == nil {
+		minimumLogLevel = &this.minimumLevel
+	}
+	if verbosityLevel == nil {
+		verbosityLevel = &this.verbosityLevel
+	}
 	return FileLogger{
-		factory: this,
-		name:    name,
+		factory:        this,
+		name:           name,
+		minimumLevel:   *minimumLogLevel,
+		verbosityLevel: *verbosityLevel,
 	}
 }
 func (this *FileLogFactory) Close() {
@@ -161,17 +179,13 @@ func (this *FileLogFactory) Close() {
 }
 
 type FileLogger struct {
-	factory *FileLogFactory
-	name    string
+	factory        *FileLogFactory
+	name           string
+	minimumLevel   LogLevel
+	verbosityLevel int
 }
 
-func (this FileLogger) CreateLogger(name string) Logger {
-	return FileLogger{
-		factory: this.factory,
-		name:    this.name + "." + name,
-	}
-}
-func (this FileLogger) log(level LogLevel, message interface{}) {
+func (this FileLogger) doLog(level LogLevel, message interface{}) {
 	rec := &LogRecord{
 		Level:     level,
 		LogSource: this.name,
@@ -181,9 +195,37 @@ func (this FileLogger) log(level LogLevel, message interface{}) {
 
 	this.factory.dispatcher <- rec
 }
-func (this FileLogger) logf(level LogLevel, format string, args ...interface{}) {
+func (this FileLogger) doLogf(level LogLevel, format string, args ...interface{}) {
 	this.log(level, CreateFormatContent(format, args...))
 }
+
+func (this FileLogger) log(level LogLevel, message interface{}) {
+	if level >= this.minimumLevel {
+		this.doLog(level, message)
+	}
+}
+func (this FileLogger) logf(level LogLevel, format string, args ...interface{}) {
+	if level >= this.minimumLevel {
+		this.doLogf(level, format, args...)
+	}
+}
+
+func (this FileLogger) CreateLogger(name string, minimumLogLevel *LogLevel, verbosityLevel *int) Logger {
+	if minimumLogLevel == nil {
+		minimumLogLevel = &this.minimumLevel
+	}
+	if verbosityLevel == nil {
+		verbosityLevel = &this.verbosityLevel
+	}
+	return FileLogger{
+		factory:        this.factory,
+		name:           this.name + "." + name,
+		minimumLevel:   *minimumLogLevel,
+		verbosityLevel: *verbosityLevel,
+	}
+}
+func (this FileLogger) V(verbosityLevel int) bool                 { return verbosityLevel >= this.verbosityLevel }
+func (this FileLogger) IsEnabled(level LogLevel) bool             { return level >= this.minimumLevel }
 func (this FileLogger) Debug(message interface{})                 { this.log(Debug, message) }
 func (this FileLogger) Debugf(format string, args ...interface{}) { this.logf(Debug, format, args...) }
 func (this FileLogger) Info(message interface{})                  { this.log(Info, message) }
@@ -194,3 +236,13 @@ func (this FileLogger) Error(message interface{})                 { this.log(Err
 func (this FileLogger) Errorf(format string, args ...interface{}) { this.logf(Error, format, args...) }
 func (this FileLogger) Fatal(message interface{})                 { this.log(Fatal, message) }
 func (this FileLogger) Fatalf(format string, args ...interface{}) { this.logf(Fatal, format, args...) }
+func (this FileLogger) Verbose(verbosityLevel int, message interface{}) {
+	if verbosityLevel >= this.verbosityLevel {
+		this.doLog(Info, message)
+	}
+}
+func (this FileLogger) Verbosef(verbosityLevel int, format string, args ...interface{}) {
+	if verbosityLevel >= this.verbosityLevel {
+		this.doLogf(Info, format, args...)
+	}
+}
